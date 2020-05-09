b359b2e045c3ac720db30e1b894ebf156bb37f94
kitakou0313 <kitakou0313@gmail.com>
2020-05-09 18:03:43 +0900

    Add chap1_9

---
+++ cracking-the-code-interview/chap1_9.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+def string_rotation(s1, s2):
+
+    s2 += s2
+
+    if (s1 in s2) and (len(s1)*2 == len(s2)):
+        return True
+
+    return False
+
+
+class Test(unittest.TestCase):
+    '''Test Cases'''
+    data = [
+        ('waterbottle', 'erbottlewat', True),
+        ('foo', 'bar', False),
+        ('foo', 'foofoo', False)
+    ]
+
+    def test_string_rotation(self):
+        for [s1, s2, expected] in self.data:
+            actual = string_rotation(s1, s2)
+            self.assertEqual(actual, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
