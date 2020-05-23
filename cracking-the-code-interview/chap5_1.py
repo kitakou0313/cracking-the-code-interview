8d3ac0c5d0e4c6469ee89d47b965ec12312dcbc1
kitakou0313 <kitakou0313@gmail.com>
2020-05-23 12:30:14 +0900

    Change some test case

--- cracking-the-code-interview/chap5_1.py
+++ cracking-the-code-interview/chap5_1.py
@@ -10,7 +10,7 @@ def insertion(N, M, i, j):
 
 class Test(unittest.TestCase):
     def test_insertion(self):
-        self.assertEqual(insertion(0b11111111, 0b10, 2, 5), 0b11001011)
+        self.assertEqual(insertion(0b11111111, 0b10, 2, 3), 0b11111011)
         self.assertEqual(insertion(0b00000000, 0b1010, 4, 7), 0b10100000)
 
 
