2c6d397377b77622e040650a9b78ebf32ec47f9a
kitakou0313 <kitakou0313@gmail.com>
2020-06-10 21:56:00 +0900

    Add chap 8_4

---
+++ cracking-the-code-interview/chap8_4.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_power_set(self):
+        s = {'a', 'b', 'c', 'd'}
+        ps = power_set(s)
+        self.assertEqual(len(ps), 16)
+        subsets = [set(), {'a'}, {'b'}, {'c'}, {'d'},
+                   {'a', 'b'}, {'a', 'c'}, {'a', 'd'}, {
+            'b', 'c'}, {'b', 'd'}, {'c', 'd'},
+            {'a', 'b', 'c'}, {'a', 'b', 'd'}, {'a', 'c', 'd'}, {'b', 'c', 'd'}, s]
+        self.assertEqual(ps, set([frozenset(s) for s in subsets]))
+
+
+if __name__ == "__main__":
+    unittest.main()
