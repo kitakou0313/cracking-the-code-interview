35684ee94aa25ebf9dd147436e64a93bfb6cee8d
kitakou0313 <kitakou0313@gmail.com>
2020-06-10 23:43:59 +0900

    Make 8_4

--- cracking-the-code-interview/chap8_4.py
+++ cracking-the-code-interview/chap8_4.py
@@ -1,6 +1,32 @@
 import unittest
 
 
+def power_set(sets):
+    N = len(sets)
+    factors = []
+    ansSets = set()
+
+    for i in range(N):
+        factors.append(sets.pop())
+
+    factors.sort()
+
+    for i in range(2 ** N):
+        tagNum = i
+        subSets = set()
+
+        ind = 0
+        while tagNum != 0:
+            if tagNum & 1 == 1:
+                subSets.add(factors[ind])
+            tagNum >>= 1
+            ind += 1
+
+        ansSets.add(frozenset(subSets))
+
+    return ansSets
+
+
 class Test(unittest.TestCase):
     def test_power_set(self):
         s = {'a', 'b', 'c', 'd'}
@@ -10,7 +36,11 @@ class Test(unittest.TestCase):
                    {'a', 'b'}, {'a', 'c'}, {'a', 'd'}, {
             'b', 'c'}, {'b', 'd'}, {'c', 'd'},
             {'a', 'b', 'c'}, {'a', 'b', 'd'}, {'a', 'c', 'd'}, {'b', 'c', 'd'}, s]
-        self.assertEqual(ps, set([frozenset(s) for s in subsets]))
+
+        for subset in subsets:
+            self.assertEqual(subset in ps, True)
+
+        #self.assertEqual(ps, set([frozenset(s) for s in subsets]))
 
 
 if __name__ == "__main__":
