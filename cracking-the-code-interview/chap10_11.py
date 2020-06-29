2639fd5c48bbe9e22c92dbde32ce1a75cc284a56
kitakou0313 <kitakou0313@gmail.com>
2020-06-29 23:44:20 +0900

    Add chap10_11 and Test case

--- cracking-the-code-interview/chap10_11.py
+++ cracking-the-code-interview/chap10_11.py
@@ -1,14 +1,33 @@
 import unittest
 
 
+def reOdrderArrayWithPeakAndValley(array):
+    "indx%2 = 0 が山、それ以外が谷とする"
+    for ind in range(len(array) - 1):
+        if ind % 2 == 0:
+            if array[ind] >= array[ind + 1]:
+                array[ind], array[ind + 1] = array[ind + 1], array[ind]
+        else:
+            if array[ind] <= array[ind + 1]:
+                array[ind], array[ind + 1] = array[ind + 1], array[ind]
+
+
 class Test(unittest.TestCase):
     def test_peaks_and_valleys(self):
         a = [12, 6, 3, 1, 0, 14, 13, 20, 22, 10]
-        peaks_and_valleys(a)
-        self.assertEqual(a, [6, 12, 1, 3, 0, 14, 13, 22, 10, 20])
+
+        reOdrderArrayWithPeakAndValley(a)
+
+        for i in range(1, len(a)-1):
+            self.assertEqual((a[i-1] <= a[i] and a[i] >= a[i + 1])
+                             or (a[i-1] >= a[i] and a[i] <= a[i + 1]), True)
+
         b = [34, 55, 60, 65, 70, 75, 85, 10, 5, 16]
-        peaks_and_valleys(b)
-        self.assertEqual(b, [34, 60, 55, 70, 65, 85, 10, 75, 5, 16])
+
+        reOdrderArrayWithPeakAndValley(b)
+        for i in range(1, len(a)-1):
+            self.assertEqual((a[i-1] <= a[i] and a[i] >= a[i + 1])
+                             or (a[i-1] >= a[i] and a[i] <= a[i + 1]), True)
 
 
 if __name__ == "__main__":
