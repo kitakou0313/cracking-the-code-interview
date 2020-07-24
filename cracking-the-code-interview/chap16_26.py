d777a7d7c60009c4fe82d8de41ced3f246ba5d47
kitakou0313 <kitakou0313@gmail.com>
2020-07-24 17:38:40 +0900

    Add chap16_26

--- cracking-the-code-interview/chap16_26.py
+++ cracking-the-code-interview/chap16_26.py
@@ -1,4 +1,56 @@
 import unittest
+import math
+
+
+def calculate(string):
+    opeAndPriorityDic = {"+": 0, "-": 0, "*": 1, "/": 1}
+    numSet = set([str(i) for i in range(0, 10)])
+
+    def collapseSrack(numberStack, operatorStack):
+        operator = operatorStack.pop()
+        number2 = numberStack.pop()
+        number1 = numberStack.pop()
+
+        if operator == "+":
+            numberStack.append(number1 + number2)
+        elif operator == "-":
+            numberStack.append(number1 - number2)
+        elif operator == "*":
+            numberStack.append(number1 * number2)
+        elif operator == "/":
+            numberStack.append(math.floor(number1 / number2))
+
+    def parseStringToNum(string, ind):
+        resNum = ""
+        while ind < len(string) and string[ind] in numSet:
+            resNum += string[ind]
+            ind += 1
+
+        return int(resNum), ind-1
+
+    numberStack = []
+    operatorStack = []
+
+    ind = 0
+    while ind < len(string):
+        if string[ind] in numSet:
+            num, ind = parseStringToNum(string, ind)
+            numberStack.append(num)
+        else:
+            if len(operatorStack) == 0:
+                operatorStack.append(string[ind])
+            elif opeAndPriorityDic[string[ind]] <= opeAndPriorityDic[operatorStack[-1]]:
+                collapseSrack(numberStack, operatorStack)
+                operatorStack.append(string[ind])
+            else:
+                operatorStack.append(string[ind])
+
+        ind += 1
+
+    while len(operatorStack) != 0:
+        collapseSrack(numberStack, operatorStack)
+
+    return numberStack[-1]
 
 
 class Test(unittest.TestCase):
