80bfed731273a1ea5772d6b99f8d23c556252409
kitakou0313 <kitakou0313@gmail.com>
2020-07-26 11:46:06 +0900

    Add chap17_1

--- cracking-the-code-interview/chap17_1.py
+++ cracking-the-code-interview/chap17_1.py
@@ -1,15 +1,29 @@
 import unittest
+import ctypes
+
+
+def addWithoutPlus(num1, num2):
+    sumRes = num1 ^ num2
+    carryBit = num1 & num2
+
+    if carryBit == 0:
+        return sumRes
+
+    carryBit = (carryBit << 1) & 0x1FFFFFFFFFFFFFFFF
+    if carryBit & (1 << 64):
+        return ctypes.c_long(sumRes).value  # for overflow
+    return addWithoutPlus(carryBit, sumRes)
 
 
 class Test(unittest.TestCase):
-    def test_add_without_plus(self):
-        self.assertEqual(add_without_plus(1, 1), 2)
-        self.assertEqual(add_without_plus(1, 2), 3)
-        self.assertEqual(add_without_plus(1001, 234), 1235)
-        self.assertEqual(add_without_plus(5, -1), 4)
-        self.assertEqual(add_without_plus(7, -5), 2)
-        self.assertEqual(add_without_plus(7, -29), -22)
-        self.assertEqual(add_without_plus(-2, 10), 8)
+    def test_addWithoutPlus(self):
+        self.assertEqual(addWithoutPlus(1, 1), 2)
+        self.assertEqual(addWithoutPlus(1, 2), 3)
+        self.assertEqual(addWithoutPlus(1001, 234), 1235)
+        self.assertEqual(addWithoutPlus(5, -1), 4)
+        self.assertEqual(addWithoutPlus(7, -5), 2)
+        self.assertEqual(addWithoutPlus(7, -29), -22)
+        self.assertEqual(addWithoutPlus(-2, 10), 8)
 
 
 if __name__ == "__main__":
